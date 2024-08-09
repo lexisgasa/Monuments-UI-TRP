1e86ca2a306ba6074ab86b513283f1ad92bc5648
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-09 23:34:48 +0200

    Create navigation menu

--- src/components/header/renderHeader.ts
+++ src/components/header/renderHeader.ts
@@ -1,10 +1,15 @@
+import { renderNavigationMenu } from "./renderNavigationMenu.js";
+
 export const renderHeader = (): HTMLElement => {
   const header = document.createElement("header");
 
   header.classList.add("header");
 
+  const navigationMenu = renderNavigationMenu();
+
   header.innerHTML = `
     <h1 class="main-heading">Monuments!</h1>
+    ${navigationMenu.outerHTML}
   `;
 
   return header;
