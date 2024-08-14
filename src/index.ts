dc5197af123b988babf8cbb89aa6250d0ac47713
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-14 16:58:28 +0200

    Add montserrat and configure evironment variable

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { getMonuments } from "./api/getMonuments.js";
 import { renderApp } from "./components/app/renderApp.js";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/700.css";
 import "./styles/styles.css";
 
 const root = document.querySelector(".root");
