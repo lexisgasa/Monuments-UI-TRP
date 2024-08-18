72a0e5ea0cb90c26c0f7f8156b879c344b1b220e
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-18 17:23:58 +0200

    Apply card styles, modularize them and fix card testing description

--- src/monument/card/renderCard.ts
+++ src/monument/card/renderCard.ts
@@ -1,9 +1,10 @@
 import type { Monument } from "../types.js";
+import "./renderCard.css";
 
 export const renderCard = (monument: Monument): HTMLElement => {
   const card = document.createElement("article");
 
-  card.classList.add("card");
+  card.classList.add("cards__card");
 
   card.innerHTML = `
     <h2>${monument.name}</h2>
--- src/monument/cardList/renderCardList.ts
+++ src/monument/cardList/renderCardList.ts
@@ -1,12 +1,16 @@
 import { renderCard } from "../card/renderCard";
 import type { Monument } from "../types";
+import "./renderCardList.css";
 
 export const renderCardList = (monuments: Monument[]): HTMLElement => {
   const cardList = document.createElement("ul");
+  cardList.classList.add("cards");
 
   const card = renderCard(monuments[0]);
 
   cardList.innerHTML = `
+    <li>${card.outerHTML}</li>
+    <li>${card.outerHTML}</li>
     <li>${card.outerHTML}</li>
   `;
 
